0fef5e9d119257015025bb2e65c8eef3f5283fd7
Andriani <valerie.andriani@gmail.com>
2020-02-24 18:05:25 +0100

    Adding model Todo + apply changes to the DB + seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+todos = Todo.create([
+    {title: "Acheter des friandises", description: "Direction Spanish Harlem pour les Chupa Chups"},
+    {title: "Faire une lessive", description: "Après la sieste de tout le monde"},
+    {title: "Appeler les impôts", description: "Dès 10h, après le rdv chez le docteur"},
+    {title: "Lorem ipsum", description: "Direction Spanish Harlem pour les Chupa Chups"},
+    {title: "RAS", description: "Ceci est un test"}
+])
